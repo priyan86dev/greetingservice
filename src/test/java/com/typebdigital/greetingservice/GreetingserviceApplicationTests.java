28b19805ed15f42416f8e6b8e28339afdd8cfd51
Priyan <priyan86dev@gmail.com>
2025-11-07 09:24:57 +0530

    initial commit

---
+++ src/test/java/com/typebdigital/greetingservice/GreetingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typebdigital.greetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
